04c231f55a7d9ff38af4b4608413b1d62a6774d0
thisisaaronland <devnull@localhost>
2020-06-10 08:55:47 -0700

    snaphot: compiled but untested NFC code

--- shoebox/ViewController.swift
+++ shoebox/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
 
-    @IBOutlet weak var scan_button: UIButton!
+    let reuseIdentifier = "reuseIdentifier"
+    var detectedMessages = [NFCNDEFMessage]()
+    var session: NFCNDEFReaderSession?
     
+    @IBOutlet weak var scan_button: UIButton!
     @IBOutlet weak var scanning_indicator: UIActivityIndicatorView!
         
     @IBOutlet weak var scanned_image: UIImageView!
-    
     @IBOutlet weak var scanned_meta: UITextView!
     
     @IBOutlet weak var save_button: UIButton!
-    
     @IBOutlet weak var clear_button: UIButton!
     
     @IBAction func clear() {
@@ -31,14 +33,120 @@ class ViewController: UIViewController {
     }
     
     @IBAction func scanTag() {
-              
-        processTag()
+        
+        guard NFCNDEFReaderSession.readingAvailable else {
+            let alertController = UIAlertController(
+                title: "Scanning Not Supported",
+                message: "This device doesn't support tag scanning.",
+                preferredStyle: .alert
+            )
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+
+        session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        session?.alertMessage = "Hold your iPhone near the item to learn more about it."
+        session?.begin()
+        ()
     }
     
-    func processTag() {
+    // MARK: - NFCNDEFReaderSessionDelegate
+
+    /// - Tag: processingTagData
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        DispatchQueue.main.async {
+            self.detectedMessages.append(contentsOf: messages)
+        }
+    }
+
+    /// - Tag: processingNDEFTag
+    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+        if tags.count > 1 {
+            // Restart polling in 500ms
+            let retryInterval = DispatchTimeInterval.milliseconds(500)
+            session.alertMessage = "More than 1 tag is detected, please remove all tags and try again."
+            DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
+                session.restartPolling()
+            })
+            return
+        }
         
-        print("PROCESS")
+        // Connect to the found tag and perform NDEF message reading
+        let tag = tags.first!
+        session.connect(to: tag, completionHandler: { (error: Error?) in
+            if nil != error {
+                session.alertMessage = "Unable to connect to tag."
+                session.invalidate()
+                return
+            }
+            
+            tag.queryNDEFStatus(completionHandler: { (ndefStatus: NFCNDEFStatus, capacity: Int, error: Error?) in
+                if .notSupported == ndefStatus {
+                    session.alertMessage = "Tag is not NDEF compliant"
+                    session.invalidate()
+                    return
+                } else if nil != error {
+                    session.alertMessage = "Unable to query NDEF status of tag"
+                    session.invalidate()
+                    return
+                }
+                
+                tag.readNDEF(completionHandler: { (message: NFCNDEFMessage?, error: Error?) in
+                    var statusMessage: String
+                    if nil != error || nil == message {
+                        statusMessage = "Fail to read NDEF from tag"
+                    } else {
+                        statusMessage = "Found 1 NDEF message"
+                        DispatchQueue.main.async {
+                            self.detectedMessages.append(message!)
+                            self.processMessage(msg: message!)
+                        }
+                    }
+                    
+                    session.alertMessage = statusMessage
+                    session.invalidate()
+                })
+            })
+        })
+    }
+    
+    /// - Tag: sessionBecomeActive
+    func readerSessionDidBecomeActive(_ session: NFCNDEFReaderSession) {
         
+    }
+    
+    /// - Tag: endScanning
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        // Check the invalidation reason from the returned error.
+        if let readerError = error as? NFCReaderError {
+            // Show an alert when the invalidation reason is not because of a
+            // successful read during a single-tag read session, or because the
+            // user canceled a multiple-tag read session from the UI or
+            // programmatically using the invalidate method call.
+            if (readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)
+                && (readerError.code != .readerSessionInvalidationErrorUserCanceled) {
+                let alertController = UIAlertController(
+                    title: "Session Invalidated",
+                    message: error.localizedDescription,
+                    preferredStyle: .alert
+                )
+                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+
+        // To read new tags, a new session instance is required.
+        self.session = nil
+    }
+    
+    func processMessage(msg: NFCNDEFMessage) {
+        
+        print("PROCESS", msg)
+        return
+
         let object_id = "18704235"
             
             let str_url = String(format: "https://collection.cooperhewitt.org/oembed/photo/?url=https://collection.cooperhewitt.org/objects/%@", object_id)
