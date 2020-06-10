f0d3aa4b3c604707cd7bece309f801ebf5ac3121
thisisaaronland <devnull@localhost>
2020-06-09 17:24:24 -0700

    Initial Commit

---
+++ shoebox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shoebox
+//
+//  Created by asc on 6/9/20.
+//  Copyright © 2020 Aaronland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
